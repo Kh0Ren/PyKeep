8f41a26107bb7c4cb4ccd93ef710f0c57bba3e0d
slava <khoren.3vium@gmail.com>
2021-08-19 15:31:06 +0300

    upgrade commit

--- Server/keepbd.py
+++ Server/keepbd.py
@@ -1,66 +1,91 @@
+# Импорт нужных библиотек для создания сервера и БД
 from flask import Flask, request
 from pony.flask import Pony
 from pony import orm
 
+# Создаю сервер
 app = Flask(__name__)
+# Интегрирую сервер с будущей БД
 Pony(app)
+# Создаю БД
 db = orm.Database()
 
 
+# Определяю сущность БД и ее атрибуты
 class Keeps(db.Entity):
+    # Первичный ключ
     id = orm.PrimaryKey(int, auto=True)
+    # Заметка пользователя
     keep = orm.Required(str)
 
 
+# Привязываю объявленную сущность к БД
 db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+# создаю таблицу БД, где будут храниться атрибуты сущности Keeps
 db.generate_mapping(create_tables=True)
 
-with orm.db_session:
-    Keeps.select().show()
 
-
+# Создаю функцию, которая будет работать со списком заметок
+# И привязываю ее к URL-адресу
+# Объявляю HTTP-методы,
 @app.route('/keeps', methods=['GET', 'POST'])
 def request_keeps():
     if request.method == 'GET':
         try:
             keeps_list = orm.select(k for k in Keeps)
-            dict = {}
+            keeps = {
+                "data": []
+            }
             for i in keeps_list:
-                dict[i.id] = i.keep
-            return dict, 200
-        except Exception:
-            return 'Error'
+                keeps["data"].append({"id": i.id, "keep": i.keep})
+            return keeps, 200
+        except Exception as error:
+            return {
+                "status": "error",
+                "message": str(error)
+            }, 500
 
     if request.method == 'POST':
         try:
-            print(request.args['keep'])
-            Keeps(keep=request.args['keep'])
-            return 'Ok'
-        except Exception:
-            return 'Error'
+            k = Keeps(keep=request.args['keep'])
+            return {"id": k.id, "keep": k.keep}, 200
+        except Exception as error:
+            return {
+                       "status": "error",
+                       "message": str(error)
+                   }, 500
 
 
-@app.route('/keeps/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-def request_keep(id):
+@app.route('/keeps/<int:keep_id>', methods=['GET', 'PUT', 'DELETE'])
+def request_keep(keep_id):
     if request.method == 'GET':
         try:
-            k = Keeps.get(id=id)
-            return k.keep
-        except Exception:
-            return 'Error 404'
+            k = Keeps[keep_id]
+            return {k.id: k.keep}, 200
+        except Exception as error:
+            return {
+                       "status": "error",
+                       "message": str(error)
+                   }, 500
     if request.method == 'PUT':
         try:
             k = Keeps.get(id=id)
             k.keep = request.args['keep']
-            return 'Ok'
-        except Exception:
-            return 'Error 404'
+            return {"status": "OK", "message": "updated"}, 200
+        except Exception as error:
+            return {
+                       "status": "error",
+                       "message": str(error)
+                   }, 500
     if request.method == 'DELETE':
         try:
             Keeps[id].delete()
-            return 'Ok'
-        except Exception:
-            return 'Error 404'
+            return {"status": "OK", "message": "deleted"}, 200
+        except Exception as error:
+            return {
+                       "status": "error",
+                       "message": str(error)
+                   }, 500
 
 
 app.run()
