4c1c58314fb1ed6176de59202723b4ed322ebee0
slava <khoren.3vium@gmail.com>
2021-08-20 16:56:46 +0300

    final commit

--- Client/keepreq.py
+++ Client/keepreq.py
@@ -5,23 +5,23 @@ import requests
 
 def createparser():
     par = argparse.ArgumentParser()
-    par.add_argument('-s', '--server')
+    par.add_argument('-s', '--server', help='server\'s URL')
 
-    subpars = par.add_subparsers(dest='command')
+    subpars = par.add_subparsers(dest='command', help='commands for interacting with keeps')
 
-    viewpar = subpars.add_parser('view')
-    viewpar.add_argument('--keep', type=int)
-    viewpar.add_argument('--keeps', action='store_const', const=1)
+    viewpar = subpars.add_parser('view', help='View keep or keeps', description='View keep or keeps')
+    viewpar.add_argument('--keep', type=int, help='view the keep by id')
+    viewpar.add_argument('--keeps', action='store_const', const=1, help='view all keeps')
 
-    addpar = subpars.add_parser('add')
-    addpar.add_argument('--keep')
+    addpar = subpars.add_parser('add', help='add keep', description='add keep')
+    addpar.add_argument('--keep', help='new keep')
 
-    delpar = subpars.add_parser('delete')
-    delpar.add_argument('--keep', type=int)
+    delpar = subpars.add_parser('delete', help='delete keep', description='delete keep')
+    delpar.add_argument('--keep', type=int, help='keep\'s id')
 
-    changepar = subpars.add_parser('change')
-    changepar.add_argument('--keep', type=int)
-    changepar.add_argument('--note')
+    changepar = subpars.add_parser('change', help='change keep', description='change keep')
+    changepar.add_argument('--keep', type=int, help='keep\'s id')
+    changepar.add_argument('--note', help='new text of keep')
 
     return par
 
@@ -35,39 +35,41 @@ if namespace.server:
         if namespace.keeps:
             r = requests.get('{}/keeps'.format(namespace.server))
             print(r.text)
-        else:
-            print('Error: required argument --keeps')
 
-        if namespace.keep:
+        elif namespace.keep:
             r = requests.get('{}/keeps/{}'.format(namespace.server, namespace.keep))
             print(r.text)
-        else:
-            print('Error: required argument --keep')
 
-    if namespace.command == 'add':
+        else:
+            print('Enter view -h to see the arguments')
+
+    elif namespace.command == 'add':
         if namespace.keep:
             r = requests.post('{}/keeps?keep={}'.format(namespace.server, namespace.keep))
             print(r.text)
-        else:
-            print('Error: required argument --keep')
 
-    if namespace.command == 'delete':
+        else:
+            print('Enter add -h to see the arguments')
+
+    elif namespace.command == 'delete':
         if namespace.keep:
             r = requests.delete('{}/keeps/{}'.format(namespace.server, namespace.keep))
             print(r.text)
-        else:
-            print('Error: required argument --keep')
 
-    if namespace.command == 'change':
+        else:
+            print('Enter delete -h to see the arguments')
+
+    elif namespace.command == 'change':
         if namespace.keep and namespace.note:
             r = requests.put('{}/keeps/{}?keep={}'.format(namespace.server, namespace.keep, namespace.note))
             print(r.text)
+
         else:
-            print('Error: required all arguments of command change')
+            print('Enter view -h to see the arguments')
 
     else:
-        print('Error: enter command')
+        parser.print_help()
 
 
 else:
-    print('Error: required argument -s/--server')
+    parser.print_help()
