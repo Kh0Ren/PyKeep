5f4f23ab971436f1dd749cbba74bb69549c71d26
slava <khoren.3vium@gmail.com>
2021-08-24 15:12:10 +0300

    create first version of keepreq test

---
+++ Client/untest.py
@@ -0,0 +1,11 @@
+import unittest
+import keepreq
+
+class MyTest(unittest.TestCase):
+
+    def test_par(self):
+        self.assertEqual(keepreq.work_parse('-s http://127.0.0.1:5000/ view --categories'), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
