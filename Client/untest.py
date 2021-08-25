d81bc699cb2dc7a8edfe71d8e111be9132c2c080
slava <khoren.3vium@gmail.com>
2021-08-25 17:48:54 +0300

    reworked test_view_cat

--- Client/untest.py
+++ Client/untest.py
@@ -4,9 +4,16 @@ import keepreq
 
 class MyTest(unittest.TestCase):
 
-    def test_par(self):
+    def setUp(self):
+        self.note_server_client = keepreq.NoteServerClient('http://127.0.0.1:5000/')
+
+    def test_view_cat(self):
         self.assertEqual(keepreq.work_parse(['-s', 'http://127.0.0.1:5000/', 'view', '--categories']), True)
 
+
+    def test_add_cat(self):
+        self.assertEqual(keepreq.work_parse(['-s', 'http://127.0.0.1:5000/', 'add', '--category', 'Expenses']), True)
+        categories, res_code = self.note_server_client.view_categories()
 
 
 
